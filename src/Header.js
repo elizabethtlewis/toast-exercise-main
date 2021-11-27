0cb79cdb62156a1b5ddf1adc4726285edc4e24ea
Beth Lewis <beth.lewis333@gmail.com>
2021-11-27 11:21:05 -0800

    Add new createFormSubmission method console.log for testing contents of form submissions

--- src/Header.js
+++ src/Header.js
@@ -7,10 +7,23 @@ import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
-
-import { createMockFormSubmission } from './service/mockServer';
+import Chance from 'chance';
+import { saveFormSubmission, fetchLikedFormSubmissions } from './service/mockServer';
 
 export default function Header() {
+  const chance = new Chance();
+  function createFormSubmission() {
+    let formSubmission = {
+      id: chance.guid(),
+      data: {
+        email: chance.email(),
+        firstName: chance.first(),
+        lastName: chance.last(),
+        liked: true,
+      },
+    };
+    return formSubmission;
+  }
   return (
     <Box sx={{flexGrow: 1}}>
       <AppBar position="static">
@@ -30,8 +43,10 @@ export default function Header() {
             variant="contained"
             size="small"
             color="secondary"
-            onClick={() => createMockFormSubmission()}
+            onClick={() => saveFormSubmission(createFormSubmission())}
           >
+            {console.log(JSON.parse(localStorage.getItem('formSubmissions')))}
+            {console.log(fetchLikedFormSubmissions())}
             New Submission
           </Button>
         </Toolbar>
