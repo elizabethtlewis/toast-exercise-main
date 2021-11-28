2c973c87ed303457612281f0805f76f99dbc470b
Beth Lewis <beth.lewis333@gmail.com>
2021-11-28 14:26:18 -0800

    Liked forms list refreshes using a custom hook added a clear all forms button

--- src/Content.js
+++ src/Content.js
@@ -1,28 +1,64 @@
 import React from 'react';
-import ReactPolling from "react-polling";
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
 import { fetchLikedFormSubmissions } from './service/mockServer';
 
 export default function Content() {
 
-  let data = (JSON.parse(localStorage.getItem('formSubmissions')) || []);
+  // Custom hook taken from https://overreacted.io/making-setinterval-declarative-with-react-hooks/
+  function useInterval(callback, delay) {
+    const savedCallback = React.useRef();
 
-  // TODO 
-  // let data = (fetchLikedFormSubmissions())
-  // add polling
-  // error handling
+    // Remember the latest function.
+    React.useEffect(() => {
+      savedCallback.current = callback;
+    }, [callback]);
+
+    // Set up the interval.
+    React.useEffect(() => {
+      function tick() {
+        savedCallback.current();
+      }
+      if (delay !== null) {
+        let id = setInterval(tick, delay);
+        return () => clearInterval(id);
+      }
+    }, [delay]);
+  }
+
+  const [likedforms, setForms] = React.useState([]);
+
+  useInterval(() => {
+    setForms(JSON.parse(localStorage.getItem('formSubmissions'))||[]);
+  }, 200);
+
+  const handleClick = () => {
+    setForms([]);
+    localStorage.clear();
+  };
 
   return (
 
     <Box sx={{marginTop: 3}}>
-
-      {/* <Typography variant="h4">Liked Form Submissions</Typography>
-      <Typography variant="body1" sx={{fontStyle: 'italic', marginTop: 1}}>
-      </Typography>  */}
-
-      {data.map(data => <p key = {data.id}>{data.data['email']}</p>)}
+      <Typography variant="h4">Liked Form Submissions</Typography>
+      <Button
+        variant="contained"
+        size="small"
+        color="error"
+        onClick={handleClick}
+      >Clear All Forms
+      </Button>
+      {likedforms.map(likedforms => 
+      <ol key = { likedforms.id }>
+        <p className = "name">
+          {likedforms.data['firstName']+" "}
+          {likedforms.data['lastName']}
+        </p>
+        <p>{likedforms.data['email']}</p>
+        <hr></hr>
+      </ol>
+      )}
     </Box>
-
   );
 }
