40dbf2ce449790722567daa2e4b962b4e1f71cdf
Beth Lewis <beth.lewis333@gmail.com>
2021-11-28 09:09:52 -0800

    Allow case where no forms have been liked

--- src/Content.js
+++ src/Content.js
@@ -1,19 +1,28 @@
 import React from 'react';
+import ReactPolling from "react-polling";
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
+import { fetchLikedFormSubmissions } from './service/mockServer';
 
 export default function Content() {
 
-  let data = (JSON.parse(localStorage.getItem('formSubmissions')));
+  let data = (JSON.parse(localStorage.getItem('formSubmissions')) || []);
+
+  // TODO 
+  // let data = (fetchLikedFormSubmissions())
+  // add polling
+  // error handling
 
   return (
+
     <Box sx={{marginTop: 3}}>
-      <Typography variant="h4">Liked Form Submissions</Typography>
-{/*
+
+      {/* <Typography variant="h4">Liked Form Submissions</Typography>
       <Typography variant="body1" sx={{fontStyle: 'italic', marginTop: 1}}>
-      </Typography> */}
-      {console.log(JSON.parse(localStorage.getItem('formSubmissions')))}
+      </Typography>  */}
+
       {data.map(data => <p key = {data.id}>{data.data['email']}</p>)}
     </Box>
+
   );
 }
